5d24d9398d302f721d75aa5e9ce4cf29bb08894e
Mutlu Akbas <mutlu.akbas1.ma@gmail.com>
2022-03-08 19:05:46 +0300

    kitap satısı eklendi

---
+++ kitap/kitap.js
@@ -0,0 +1,16 @@
+function satinAl() {
+
+    var element = document.getElementById("image");
+    element.style.backgroundColor= "blue";
+
+
+
+    var element = document.getElementById("mesaj");
+    element.innerHTML="<h1>Kitap Satın Alındı</h2>";
+
+
+
+
+    console.log("Kitap Satın Alındı");
+}
+
