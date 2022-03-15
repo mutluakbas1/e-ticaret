cf9a1dd4adb52ca5987d977a0194e4c0d435e8d0
Mutlu Akbas <mutlu.akbas1.ma@gmail.com>
2022-03-15 15:59:03 +0300

    saat degisimleri eklendi

---
+++ saat/saat.js
@@ -0,0 +1,45 @@
+function siyahsatinal() {
+
+
+    var element = document.getElementById('saatdegisimi');
+    element.src = "resim/clsaat-06-2_urun_g2981_900x900_H36yNYfV.jpg";
+
+
+    var element = document.getElementById('mesaj1');
+    element.innerHTML="Siyah Saat";
+
+
+
+    var element = document.getElementById('fiyat1');
+    element.innerHTML="20.000";
+
+   
+}
+
+function mavisatinal() {
+
+    var element = document.getElementById('saatdegisimi');
+    element.src = "resim/11179005182002.jpg";
+
+
+    var element = document.getElementById('mesaj1');
+    element.innerHTML="Mavi Saat";
+
+    var element = document.getElementById('fiyat1');
+    element.innerHTML="25.000";
+}
+function kırmızısatinal() {
+
+
+    var element = document.getElementById('saatdegisimi');
+    element.src = "resim/633092960_tn50_0.jpg";
+
+
+    var element = document.getElementById('mesaj1');
+    element.innerHTML="Kırmızı Saat";
+
+
+    var element = document.getElementById('fiyat1');
+    element.innerHTML="30.000";
+
+}
