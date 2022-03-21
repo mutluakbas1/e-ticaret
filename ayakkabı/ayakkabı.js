d4b13d8f4fd430f310c05e5b910d5415af21a102
Mutlu Akbas <mutlu.akbas1.ma@gmail.com>
2022-03-22 00:33:05 +0300

    ayakkabı markaları olusturuldu

---
+++ ayakkabı/ayakkabı.js
@@ -0,0 +1,45 @@
+function nikesatinal() {
+
+
+    var element = document.getElementById('ayakkabidegisimi');
+    element.src = "images/air-force-1-big-kids-shoes-M7mcL9.jpg";
+
+
+    var element = document.getElementById('mesaj1');
+    element.innerHTML="NİKE";
+
+
+
+    var element = document.getElementById('fiyat1');
+    element.innerHTML="700 TL";
+
+   
+}
+
+function adidassatinal() {
+
+    var element = document.getElementById('ayakkabidegisimi');
+    element.src = "images/adidas_x_LEGO(r)_Superstar_Ayakkabi_Beyaz_FZ8497_01_standard.jpg";
+
+
+    var element = document.getElementById('mesaj1');
+    element.innerHTML="ADİDAS";
+
+    var element = document.getElementById('fiyat1');
+    element.innerHTML="450 TL";
+}
+function pumasatinal() {
+
+
+    var element = document.getElementById('ayakkabidegisimi');
+    element.src = "images/png.jpg";
+
+
+    var element = document.getElementById('mesaj1');
+    element.innerHTML="PUMA";
+
+
+    var element = document.getElementById('fiyat1');
+    element.innerHTML="200 TL";
+
+}
