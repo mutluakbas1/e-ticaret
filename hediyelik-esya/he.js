46ad39a3160ac57a618d27c495af4de83fdff4cc
Mutlu Akbas <mutlu.akbas1.ma@gmail.com>
2022-03-22 01:32:08 +0300

    hediyelik esyalar eklendi

---
+++ hediyelik-esya/he.js
@@ -0,0 +1,45 @@
+function cikolatasatinal() {
+
+
+    var element = document.getElementById('hedegisimi');
+    element.src = "he/hediyelik-special-cikolata-iyi-bayramlar-buyuk-boy-bayram-cikolatasi-2011-34-O.jpg";
+
+
+    var element = document.getElementById('mesaj1');
+    element.innerHTML="Cikolata";
+
+
+
+    var element = document.getElementById('fiyat1');
+    element.innerHTML="500 TL";
+
+   
+}
+
+function ciceksatinal() {
+
+    var element = document.getElementById('hedegisimi');
+    element.src = "he/hediye-kutusu_1.jpg";
+
+
+    var element = document.getElementById('mesaj1');
+    element.innerHTML="Cicek";
+
+    var element = document.getElementById('fiyat1');
+    element.innerHTML="250 TL";
+}
+function kalemsatinal() {
+
+
+    var element = document.getElementById('hedegisimi');
+    element.src = "he/dinc-ozel-hediyelik-kalem-550x550.jpg";
+
+
+    var element = document.getElementById('mesaj1');
+    element.innerHTML="Kalem";
+
+
+    var element = document.getElementById('fiyat1');
+    element.innerHTML="150 TL";
+
+}
