5f2ac16d423f2714a63a7aa62760615d7df72635
Mutlu Akbas <mutlu.akbas1.ma@gmail.com>
2022-03-22 01:12:47 +0300

    telefon markaları eklendi

---
+++ telefon/telefon.js
@@ -0,0 +1,45 @@
+function iphonesatinal() {
+
+
+    var element = document.getElementById('telefondegisimi');
+    element.src = "tel/iphone-12-white-select-2020.png";
+
+
+    var element = document.getElementById('mesaj1');
+    element.innerHTML="İPHONE";
+
+
+
+    var element = document.getElementById('fiyat1');
+    element.innerHTML="15.000 TL";
+
+   
+}
+
+function samsungsatinal() {
+
+    var element = document.getElementById('telefondegisimi');
+    element.src = "tel/1_org.jpg";
+
+
+    var element = document.getElementById('mesaj1');
+    element.innerHTML="SAMSUNG";
+
+    var element = document.getElementById('fiyat1');
+    element.innerHTML="7500 TL";
+}
+function sonysatinal() {
+
+
+    var element = document.getElementById('telefondegisimi');
+    element.src = "tel/v2-68915-1_large.jpg";
+
+
+    var element = document.getElementById('mesaj1');
+    element.innerHTML="SONY";
+
+
+    var element = document.getElementById('fiyat1');
+    element.innerHTML="4000 TL";
+
+}
